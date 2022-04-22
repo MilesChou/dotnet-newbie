7363a629702589be3a061163367e33870683b558
MilesChou <jangconan@gmail.com>
2022-04-23 01:41:35 +0800

    Complete course 1

---
+++ Courses/Courses/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Courses
+{
+    public class Course_1
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(args[0], out var score);
+
+            Console.WriteLine(Grade(score));
+        }
+
+        public static string Grade(int score) => score switch
+        {
+            > 100 => "分數錯誤，請重新輸入",
+            >= 90 => "考試優秀",
+            >= 80 => "考試良好",
+            >= 60 => "考試及格",
+            >= 0 => "下次繼續努力",
+            _ => "分數錯誤，請重新輸入",
+        };
+    }
+}
---
+++ Courses/Tests/UnitTest.cs
@@ -0,0 +1,28 @@
+using Courses;
+using NUnit.Framework;
+
+namespace Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    [TestCase("分數錯誤，請重新輸入", 101)]
+    [TestCase("分數錯誤，請重新輸入", -1)]
+    [TestCase("考試優秀", 100)]
+    [TestCase("考試優秀", 90)]
+    [TestCase("考試良好", 89)]
+    [TestCase("考試良好", 80)]
+    [TestCase("考試及格", 79)]
+    [TestCase("考試及格", 60)]
+    [TestCase("下次繼續努力", 59)]
+    [TestCase("下次繼續努力", 0)]
+    public void TestCourse1(string excepted, int score)
+    {
+        Assert.AreEqual(excepted, Course_1.Grade(score));
+    }
+}
