b873000bbb0dec3d00f81f56a461cc32d596452c
YeezhiChicken <hibary.taiwan@gmail.com>
2022-07-10 23:11:49 +0800

    refactor: 重構Course03Tests 排序方式

--- Courses/Course03/Program.cs
+++ Courses/Course03/Program.cs
@@ -11,6 +11,8 @@
             Console.WriteLine($"{a} , {b} , {c}");
 
             Console.WriteLine($"中間數是 {Find(a, b, c)}");
+
+            Console.WriteLine($"中間數是 {NewFind(a, b, c)}");
         }
 
         public static float Find(float a, float b, float c)
@@ -31,6 +33,23 @@
             }
 
             return 0;
+        }
+
+        /// <summary>
+        /// 做排序後，取中間位
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns>三數中的中間數</returns>
+        public static float NewFind(float a, float b, float c)
+        {
+            IEnumerable<float> floats = new List<float>()
+            {
+                a, b, c
+            };
+
+            return floats.OrderBy(x => x).ToList()[1];
         }
     }
 }
--- Courses/Course03Tests/UnitTest.cs
+++ Courses/Course03Tests/UnitTest.cs
@@ -20,4 +20,16 @@ public class Tests
     {
         Assert.AreEqual(2, Course03.Course03.Find(a, b, c));
     }
+
+    [Test]
+    [TestCase(1, 2, 3)]
+    [TestCase(1, 3, 2)]
+    [TestCase(3, 1, 2)]
+    [TestCase(2, 3, 1)]
+    [TestCase(2, 1, 3)]
+    [TestCase(3, 2, 1)]
+    public void NewFind_三數取中間值(float a, float b, float c)
+    {
+        Assert.AreEqual(2, Course03.Course03.NewFind(a, b, c));
+    }
 }
